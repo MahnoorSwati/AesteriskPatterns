05825a08bcdce9aca8a5a77f85e6f262aa162e03
Mahnoor Khan <mahnoorkhan_hallian@yahoo.com>
2024-05-22 11:49:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+"""for i in range(1,6):
+  for j in range(i):
+   print("*", end="")
+  print("")
+print("\n")
+
+for i in range(1,6):
+  for j in range(i):
+   print(i, end="")
+  print("")
+print("\n")
+
+for i in range(1,6):
+  for j in range(1, i+1):
+   print(j, end="")
+  print("")
+print("\n")
+
+for i in range(1,6):
+  for j in range(1,6):
+    if(j==i):
+      print("*", end="")
+    else: 
+      print(" ", end="")
+  print("")
+print("\n")
+
+for i in range(1,6):
+  for j in range(1,6):
+      print("*", end="")
+  print("")
+print("\n")"""
+
+for i in range(6, 0,-1):
+  for j in range(i):
+    print("*", end="")
+  print("")
+
